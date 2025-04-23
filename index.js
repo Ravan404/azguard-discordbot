4e7a36bffb094661578845c13676edb2f76ba23f
RevanQasimov <73063908+Ravan404@users.noreply.github.com>
2025-04-23 06:24:32 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Bot olarak giriş yapıldı: ${client.user.tag}`);
+});
+
+client.on('messageCreate', async message => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    await message.reply('Pong! 🏓');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
