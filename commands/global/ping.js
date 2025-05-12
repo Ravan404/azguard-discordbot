a2d05a576f3abc638d0e881f2986ea05ff0762fc
Ravan404 <73063908+Ravan404@users.noreply.github.com>
2025-05-13 00:54:16 +0400

    fixes

--- commands/global/ping.js
+++ commands/global/ping.js
@@ -1,15 +1,15 @@
-const { SlashCommandBuilder, PermissionFlagsBits} = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-      .setName('ping')
-      .setDescription('Botun gecikmə müddətini yoxlayır')
-      .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
-  async execute(interaction) {
-    // Botun gecikmə müddətini hesablayırıq
-    const sent = await interaction.reply({ content: 'Ping hesablanır', fetchReply: true });
-    const ping = sent.createdTimestamp - interaction.createdTimestamp;
-    
-    await interaction.editReply(`Bot gecikməsi: ${ping}ms\nAPI gecikməsi: ${interaction.client.ws.ping}ms`);
-  },
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Botun gecikmə müddətini yoxlayır')
+        .setDefaultMemberPermissions('0'),
+
+    async execute(interaction) {
+        const sent = await interaction.reply({ content: 'Ping hesablanır...', fetchReply: true });
+        const ping = sent.createdTimestamp - interaction.createdTimestamp;
+        
+        await interaction.editReply(`Bot gecikməsi: ${ping}ms\nAPI gecikməsi: ${interaction.client.ws.ping}ms`);
+    },
 };
