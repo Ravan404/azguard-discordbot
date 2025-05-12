cdfe112b65ebd4cb823a9af34ec9d498e096c155
Ravan404 <73063908+Ravan404@users.noreply.github.com>
2025-05-13 00:49:38 +0400

    fixes

--- commands/global/ping.js
+++ commands/global/ping.js
@@ -7,7 +7,7 @@ module.exports = {
       .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
   async execute(interaction) {
     // Botun gecikmə müddətini hesablayırıq
-    const sent = await interaction.reply({ content: 'Ping hesablanır...', fetchReply: true });
+    const sent = await interaction.reply({ content: 'Ping hesablanır', fetchReply: true });
     const ping = sent.createdTimestamp - interaction.createdTimestamp;
     
     await interaction.editReply(`Bot gecikməsi: ${ping}ms\nAPI gecikməsi: ${interaction.client.ws.ping}ms`);
