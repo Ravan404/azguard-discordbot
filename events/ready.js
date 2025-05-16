e39aafe6dded50a6762fb2cfbdf33397cc0af32e
Ravan404 <73063908+Ravan404@users.noreply.github.com>
2025-05-16 10:38:58 +0400

    activity v1.0fix

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,5 @@ module.exports = {
   once: true,
   execute(client) {
     console.log(`Bot aktiv olaraq qoşuldu: ${client.user.tag}`);
-    client.user.setActivity('VALORANT', { type: ActivityType.Playing });
   },
 };
