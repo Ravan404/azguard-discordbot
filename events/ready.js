82c07cd03baaf2199aa54e814b72a0f1016c980e
Ravan404 <73063908+Ravan404@users.noreply.github.com>
2025-05-16 10:37:09 +0400

    activity

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,6 @@ module.exports = {
   once: true,
   execute(client) {
     console.log(`Bot aktiv olaraq qoşuldu: ${client.user.tag}`);
+    client.user.setActivity('VALORANT', { type: ActivityType.Playing });
   },
 };
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, GatewayIntentBits, Collection, ActivityType } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 const dotenv = require('dotenv');
